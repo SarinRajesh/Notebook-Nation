b0b59de9e38b83d14580bc03b3defac060f0973d
SarinRajesh <77143396+SarinRajesh@users.noreply.github.com>
2025-12-06 11:25:10 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,4 +1,17 @@
-<?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'miniproject');
-    if(!$conn) die('error connecting to database')
+<?php
+$conn = mysqli_connect(
+    "sql105.infinityfree.com",   // MySQL Host Name
+    "if0_40611957",              // MySQL User Name
+    "Sarin0606",      // MySQL Password
+    "if0_40611957_miniproject"   // Database name
+);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
+
+ 
+ #   $conn = mysqli_connect('localhost', 'root', '', 'miniproject');
+#    if(!$conn) die('error connecting to database')
 ?>
